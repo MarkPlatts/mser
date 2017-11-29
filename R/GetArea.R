f64dbe4c043e7820936e2643baa0da2f0eeceb4e
Mark Platts <markplatts23@gmail.com>
2017-11-29 15:33:57 +0000

    Added get_area to the namespace and improved the meaning of the argument name plot.path by changing it to path.

--- R/GetArea.R
+++ R/GetArea.R
@@ -1,12 +1,12 @@
 #' Get Area
 #'
-#' @param plot.path
+#' @param path the path to where the StockAreas.csv file is held
 #' @param file.name the name of the result file that is being plotted
 #' @param area the area represented by the functional group found in the result file name
 #'
 #' @return the area as a numeric(1)
 #' @export
-get_area <- function(plot.path, file.name, area){
+get_area <- function(path, file.name, area){
   stock.areas.file.path <- paste0(plot.path, "StockAreas.csv")
   if(file.exists(stock.areas.file.path)){
     stock.areas <- read.csv(stock.areas.file.path, stringsAsFactors = F)

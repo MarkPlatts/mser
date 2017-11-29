12249018a2ecd7d0094f2b519af349cfc0917004
Mark Platts <markplatts23@gmail.com>
2017-11-29 15:30:11 +0000

    Added the get area function from the share_tools.R source code for the old plotting code.

---
+++ R/GetArea.R
@@ -0,0 +1,19 @@
+#' Get Area
+#'
+#' @param plot.path
+#' @param file.name the name of the result file that is being plotted
+#' @param area the area represented by the functional group found in the result file name
+#'
+#' @return the area as a numeric(1)
+#' @export
+get_area <- function(plot.path, file.name, area){
+  stock.areas.file.path <- paste0(plot.path, "StockAreas.csv")
+  if(file.exists(stock.areas.file.path)){
+    stock.areas <- read.csv(stock.areas.file.path, stringsAsFactors = F)
+    n.areas <- dim(stock.areas)[1]
+    for(irow in 1:n.areas){
+      if(str_detect(str_to_upper(file.name), str_to_upper(stock.areas[irow,1]))) return (stock.areas[irow, 2])
+    }
+  }
+  return (area)
+}
